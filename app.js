70c150d7379003f052f700ca4920d4042af7ab81
Eihab <mastereihab@gmail.com>
2020-08-31 22:46:30 +0100

    added lyrics retrieval functionality

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const API_URL = "https://api.lyrics.ovh"
 async function searchSongs(term) {
     const res = await fetch(`${API_URL}/suggest/${term}`)
     const data = await res.json();
-    showData(data);
+    showData(data);  
 }
 
 //  Display Song Info in DOM
@@ -20,7 +20,7 @@ function showData(data) {
         .map(
           song => `<li>
       <span><strong>${song.artist.name}</strong> - ${song.title}</span>
-      <button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}">Get Lyrics</button>
+      <button class="btn" data-artist='${song.artist.name}' data-songtitle='${song.title}'>Get Lyrics</button>
     </li>`
         )
         .join('')}
@@ -46,6 +46,17 @@ async function getMoreSongs(url) {
     showData(data);
 }
 
+async function getLyrics(artist, songTitle) {
+    const res = await fetch(`${API_URL}/v1/${artist}/${songTitle}`)
+    const data = await res.json();
+    const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, "<br>");
+    result.innerHTML = `
+    <h2><strong>${artist}</strong> - ${songTitle}</h2>
+    <span>${lyrics}</span>
+    `
+    more.innerHTML = ""
+}
+
 // Event Listeners
 form.addEventListener("submit", e => {
     e.preventDefault();
@@ -57,3 +68,13 @@ form.addEventListener("submit", e => {
         alert("Can't search for nothing 🤔");
     }
 })
+
+
+result.addEventListener("click", e => {
+    if (e.target.tagName === "BUTTON") {
+        const clickedElement = e.target;
+        const artist = clickedElement.getAttribute("data-artist")
+        const songTitle = clickedElement.getAttribute("data-songtitle")
+        getLyrics(artist, songTitle)
+    }
+});
